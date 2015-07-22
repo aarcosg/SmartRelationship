b8fbff3e9dfb731077565d3f67f89cb2e98bc66d
Alvaro Arcos <arcos1989@gmail.com>
2015-07-22 12:30:58 +0200

    Deleted looper and media recorder callback in audio recorder service.

--- app/src/main/java/us/idinfor/smartrelationship/audio/AudioRecorderService.java
+++ app/src/main/java/us/idinfor/smartrelationship/audio/AudioRecorderService.java
@@ -3,7 +3,6 @@ package us.idinfor.smartrelationship.audio;
 import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Environment;
-import android.os.Looper;
 import android.util.Log;
 
 import java.io.File;
@@ -44,40 +43,17 @@ public class AudioRecorderService extends WakefulIntentService{
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         recorder.setOutputFile(path);
         recorder.setMaxDuration(Utils.getSharedPreferences(this).getInt(Constants.PROPERTY_VOICE_RECORD_DURATION, Constants.DEFAULT_VOICE_RECORD_DURATION) * 1000);
-        recorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-            @Override
-            public void onInfo(MediaRecorder mr, int what, int extra) {
-                Log.i(TAG,"MediaRecorder callback");
-                switch (what){
-                    case MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED:
-                        Log.i(TAG,"MediaRecorder callback = MEDIA_RECORDER_INFO_MAX_DURATION_REACHED");
-                        mr.reset();
-                        mr.release();
-                        if(Looper.myLooper()!= null){
-                            Looper.myLooper().quit();
-                        }
-                        break;
-                }
-
-            }
-        });
-
 
         try {
             recorder.prepare();
+            recorder.start();
         } catch (IOException e) {
             //Log.e(TAG, e.getMessage());
             e.printStackTrace();
-        }
-
-        try {
-            recorder.start();
-        } catch (Exception e) {
+        } catch (IllegalStateException e) {
             //Log.e(TAG, e.getMessage());
             e.printStackTrace();
         }
-
-        Looper.loop();
 
     }
 }
