abbecd3952d97bde3c8c4cea654283b5aaa4860f
Alvaro Arcos <arcos1989@gmail.com>
2015-10-22 13:54:55 +0200

    Version 1.6 Enable audio recorder by default. Disable audio recorder switch button.

--- app/src/main/java/us/idinfor/smartrelationship/SmartRelationshipApplication.java
+++ app/src/main/java/us/idinfor/smartrelationship/SmartRelationshipApplication.java
@@ -11,6 +11,7 @@ public class SmartRelationshipApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Utils.getSharedPreferences(this).edit().putBoolean(Constants.PROPERTY_RECORD_AUDIO_ENABLED, true).commit();
         Fabric.with(this, new Crashlytics());
     }
 }
